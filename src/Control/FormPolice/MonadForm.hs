8e302386c436fb3d513129594be89f5df8a966dc
Roman Gonzalez <romanandreg@gmail.com>
2011-01-28 22:24:32 -0800

    Adding the MonadForm module

---
+++ src/Control/FormPolice/MonadForm.hs
@@ -0,0 +1,14 @@
+module Control.FormPolice.MonadForm where
+
+  import           Data.Text (Text)
+  import           Data.Aeson (FromJSON)
+  import           Data.Monoid (Monoid)
+  
+  class MonadForm m where
+    text     :: (Monoid a, FromJSON a) => Text -> m a 
+    password :: (Monoid a, FromJSON a) => Text -> m a
+    textarea :: (Monoid a, FromJSON a) => Text -> m a
+    checkbox :: (Monoid a, FromJSON a) => Text -> m a
+    radio    :: (Monoid a, FromJSON a) => Text -> [(Text, Text)] -> m a
+    select   :: (Monoid a, FromJSON a) => Text -> [(Text, Text)] -> m a
+    
